d2d122a684fa55c009cb74422adbf55883a4e44f
AshrafAaref21 <ashrafaaref2020@gmail.com>
2024-06-02 16:47:12 +0300

    test

--- app/app/settings.py
+++ app/app/settings.py
@@ -92,7 +92,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
-        'PORT': '5432',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),

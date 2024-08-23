8cc2502a8ff3773346e966e201aa78103a0a3d43
Ashraf Aaref <82468597+AshrafAaref21@users.noreply.github.com>
2024-08-23 15:39:33 +0300

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -164,5 +164,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ashrafaaref2020@gmail.com'
-EMAIL_HOST_PASSWORD = 'blybexidakheqafk'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'changeme')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'changeme')
